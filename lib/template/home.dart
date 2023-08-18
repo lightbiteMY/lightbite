870131cbc99d163f929ec8fe39fe8f3b1ff6f174
K M <30794641+kam89@users.noreply.github.com>
2023-08-18 18:06:39 +0800

    🎨 Update HomeTemplate

--- lib/template/home.dart
+++ lib/template/home.dart
@@ -4,76 +4,31 @@ class HomeTemplate extends StatelessWidget {
   const HomeTemplate({
     super.key,
     required this.title,
-    required this.currentPageIndex,
-    required this.onDestinationSelected,
   });
 
   final String title;
-  final int currentPageIndex;
-  final Function onDestinationSelected;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(title),
-        ),
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected: (int index) {
-            onDestinationSelected(index);
-          },
-          selectedIndex: currentPageIndex,
-          destinations: const <Widget>[
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(
-                icon: Icon(Icons.mail), label: 'Notification'),
-            NavigationDestination(icon: Icon(Icons.people), label: 'Profile')
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have clicked the button this many times:',
+            ),
+            Text(
+              '1',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
-        body: <Widget>[
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have clicked the button this many times:',
-                ),
-                Text(
-                  '1',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Notification',
-                ),
-                Text(
-                  '2',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Profile',
-                ),
-                Text(
-                  '3',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-        ][currentPageIndex]);
+      ),
+    );
   }
 }
