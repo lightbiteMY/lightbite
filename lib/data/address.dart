6d10a298294fcb2f01461896ea82260442e0979d
K.M WONG <30794641+kam89@users.noreply.github.com>
2023-09-14 02:52:06 +0800

    Create address.dart
    Create a new address class
    - may not require this
    - work in progress

---
+++ lib/data/address.dart
@@ -0,0 +1,44 @@
+class Address {
+  final String? name;
+  final String? line1;
+  final String? line2;
+  final String? line3;
+  final String? line4;
+  final String? city;
+  final String? postcode;
+  final String? state;
+  final ({double x, double y})? coordinate;
+
+  const Address(
+    this.name,
+    this.line1,
+    this.line2,
+    this.line3,
+    this.line4,
+    this.city,
+    this.postcode,
+    this.state,
+    this.coordinate,
+  );
+
+  Map<String, dynamic> toMap() => {
+        'name': name,
+        'line1': line1,
+        'line2': line2,
+        'line3': line3,
+        'line4': line4,
+        'city': city,
+        'postcode': postcode,
+        'state': state,
+        'coordinate': coordinate,
+      };
+
+  operator [](String field) => toMap()[field];
+
+  String? get getFullAddress {
+    if (name == null) return null;
+    return '$line1, ${line2 ?? ''}, ${line3 ?? ''}, $city, $postcode, $state';
+  }
+
+  String? get getName => name;
+}
