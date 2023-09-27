5a3dfcfd89d6229515afbe919c7b7587283379f2
K M <30794641+kam89@users.noreply.github.com>
2023-09-27 18:41:46 +0800

    Update restaurant_model.dart

--- lib/models/restaurant_model.dart
+++ lib/models/restaurant_model.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+List<RestaurantModel> restaurantListModelFromJson(String data) =>
+    List<RestaurantModel>.from(json.decode(data).map((x) => RestaurantModel));
+
 class RestaurantModel {
   final String name;
   final ({double x, double y}) coordinate;
@@ -5,4 +10,18 @@ class RestaurantModel {
   final String imageUrl;
 
   const RestaurantModel(this.name, this.coordinate, this.tags, this.imageUrl);
+
+  // RestaurantModel.fromJson(Map<String, dynamic> json) {
+  //   name = json["name"];
+  //   coordinate = json["coordinate"];
+  //   tags = json["tags"];
+  //   imageUrl = json["imageUrl"];
+  // }
+
+  // Map<String, dynamic> toJson() => {
+  //       "name": name,
+  //       "coordinate": coordinate,
+  //       "tags": tags,
+  //       "imageUrl": imageUrl,
+  //     };
 }
