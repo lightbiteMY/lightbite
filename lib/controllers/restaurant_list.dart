0c10104a32dc9a97f32037d93951aafd0566e260
K M <30794641+kam89@users.noreply.github.com>
2023-09-27 16:34:20 +0800

    Move restaurant_list.dart to folder controllers

--- lib/controllers/restaurant_list.dart
+++ lib/controllers/restaurant_list.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 
 import 'package:flutter/widgets.dart';
-import 'package:lightbite/data/restaurant_model.dart';
+import 'package:lightbite/models/restaurant_model.dart';
 
 class RestaurantList extends ChangeNotifier {
   final List<RestaurantModel> _restaurantList = [];
