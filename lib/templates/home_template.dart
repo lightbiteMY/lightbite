44b9c51340e65a8e25ae992274af1807c2177d48
K M <30794641+kam89@users.noreply.github.com>
2023-09-27 16:33:35 +0800

    Rename folder template to templates

--- lib/templates/home_template.dart
+++ lib/templates/home_template.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lightbite/data/restaurant_model.dart';
+import 'package:lightbite/models/restaurant_model.dart';
 import 'package:lightbite/molecule/restaurant_card.dart';
 
 class HomeTemplate extends StatelessWidget {
