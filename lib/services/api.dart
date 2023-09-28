0c266d8c90ef95e5b3edd1ff02e83a3a53171e50
K.M WONG <30794641+kam89@users.noreply.github.com>
2023-09-29 02:47:20 +0800

    Update api.dart, remove unused code
    - to call the api from localhost express server
    - to remove hardcoded data

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,12 +4,10 @@ import 'package:lightbite/models/restaurant_model.dart';
 class ApiService {
   Future<List<RestaurantModel>> getRestaurantList() async {
     var response =
-        await http.get(Uri.parse("https://localhost/getRestaurantList"));
+        await http.get(Uri.parse("http://localhost:3000/getRestaurantList"));
 
     if (response.statusCode == 200) {
-      // var data = response.body;
-      const data =
-          '[{"name": "SDS", "coordinate":{x: 2.964994, y: 101.7721517}, "tags":["Chinese", "FastFood", "Healthy"], "imageUrl":"https://sdsgroups.com/wp-content/uploads/2021/11/cropped-Asset-3-e1637812026983.png"}]';
+      var data = response.body;
       return restaurantListModelFromJson(data);
     }
     throw Exception();
