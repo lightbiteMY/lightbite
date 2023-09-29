6a96e3ef4ab8af34af3351e4ce84ba0c74c37d69
K M <30794641+kam89@users.noreply.github.com>
2023-09-29 16:13:15 +0800

    Refactor due to rename

--- lib/providers/favourite_place_list_provider.dart
+++ lib/providers/favourite_place_list_provider.dart
@@ -2,7 +2,7 @@ import 'dart:collection';
 
 import 'package:flutter/widgets.dart';
 import 'package:lightbite/models/address_model.dart';
-import 'package:lightbite/services/favourite_place_api.dart';
+import 'package:lightbite/services/personal_api.dart';
 
 class FavouritePlaceListProvider extends ChangeNotifier {
   List<AddressModel> _favouritePlaceList = [];
@@ -10,7 +10,7 @@ class FavouritePlaceListProvider extends ChangeNotifier {
   UnmodifiableListView<AddressModel> get favouritePlaceList =>
       UnmodifiableListView(_favouritePlaceList);
 
-  AddressApiService api = AddressApiService();
+  PersonalApiService api = PersonalApiService();
 
   getFavouritePlaceList() async {
     _favouritePlaceList = await api.getFavouritePlaceList();
