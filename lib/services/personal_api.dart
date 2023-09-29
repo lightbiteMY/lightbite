3c2bb615b47ed6397c3ad39114b5e9546617c48e
K M <30794641+kam89@users.noreply.github.com>
2023-09-29 16:13:03 +0800

    Rename to personal_api.dart

--- lib/services/personal_api.dart
+++ lib/services/personal_api.dart
@@ -1,14 +1,14 @@
 import 'package:http/http.dart' as http;
 import 'package:lightbite/models/address_model.dart';
 
-class AddressApiService {
-  Future<List<AddressModel>> getFavouriteAddressList() async {
+class PersonalApiService {
+  Future<List<AddressModel>> getFavouritePlaceList() async {
     var response = await http.get(
-        Uri.parse("http://localhost:3000/personal/v1/getFavouriteAddressList"));
+        Uri.parse("http://localhost:3000/personal/v1/getFavouritePlaceList"));
 
     if (response.statusCode == 200) {
       var data = response.body;
-      return favouriteAddressListModelFromJson(data);
+      return favouritePlaceListModelFromJson(data);
     }
     throw Exception();
   }
