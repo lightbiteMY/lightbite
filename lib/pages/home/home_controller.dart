dacfbaf8f62a69fd28f93db59433ecd5c5ed9f63
K M <30794641+kam89@users.noreply.github.com>
2023-10-03 16:52:33 +0800

    Rename files for home page

---
+++ lib/pages/home/home_controller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:lightbite/providers/favourite_place_list_provider.dart';
+import 'package:lightbite/providers/restaurant_list_provider.dart';
+import 'package:lightbite/pages/home_template.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedFavouritePlace = 0;
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<RestaurantListProvider>(context, listen: false)
+        .getRestaurantList();
+    Provider.of<FavouritePlaceListProvider>(context, listen: false)
+        .getFavouritePlaceList();
+  }
+
+  void changeFavouritePlace(value) {
+    setState(() {
+      selectedFavouritePlace = value;
+    });
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final restaurantListProvider = Provider.of<RestaurantListProvider>(context);
+    final favouritePlaceListProvider =
+        Provider.of<FavouritePlaceListProvider>(context);
+
+    return HomeTemplate(
+      restaurants: restaurantListProvider.restaurantList,
+      favouritePlaces: favouritePlaceListProvider.favouritePlaceList,
+      selectedFavouritePlace: selectedFavouritePlace,
+      changeFavouritePlace: changeFavouritePlace,
+    );
+  }
+}
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,45 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:lightbite/providers/favourite_place_list_provider.dart';
-import 'package:lightbite/providers/restaurant_list_provider.dart';
-import 'package:lightbite/pages/home_template.dart';
-import 'package:provider/provider.dart';
+import 'package:lightbite/models/address_model.dart';
+import 'package:lightbite/models/restaurant_model.dart';
+import 'package:lightbite/pages/home/widgets/restaurant_card_widget.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomeTemplate extends StatelessWidget {
+  final String defaultInstruction = 'Please Set Your Address';
+  final List<RestaurantModel> restaurants;
+  final List<AddressModel> favouritePlaces;
+  final int selectedFavouritePlace;
+  final Function changeFavouritePlace;
+  const HomeTemplate(
+      {super.key,
+      required this.restaurants,
+      required this.favouritePlaces,
+      required this.selectedFavouritePlace,
+      required this.changeFavouritePlace});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int selectedFavouritePlace = 0;
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<RestaurantListProvider>(context, listen: false)
-        .getRestaurantList();
-    Provider.of<FavouritePlaceListProvider>(context, listen: false)
-        .getFavouritePlaceList();
-  }
-
-  void changeFavouritePlace(value) {
-    setState(() {
-      selectedFavouritePlace = value;
-    });
-    Navigator.of(context).pop();
+  void showFavouritePlaceModal(context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView(
+          padding: const EdgeInsets.all(8),
+          children: favouritePlaces
+              .map((place) => RadioListTile(
+                    groupValue: selectedFavouritePlace,
+                    onChanged: (value) => changeFavouritePlace(value),
+                    value: favouritePlaces.indexOf(place),
+                    title: Text(
+                      place.name!,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      place.getFullAddress!,
+                      style: const TextStyle(fontSize: 10),
+                    ),
+                  ))
+              .toList(),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final restaurantListProvider = Provider.of<RestaurantListProvider>(context);
-    final favouritePlaceListProvider =
-        Provider.of<FavouritePlaceListProvider>(context);
-
-    return HomeTemplate(
-      restaurants: restaurantListProvider.restaurantList,
-      favouritePlaces: favouritePlaceListProvider.favouritePlaceList,
-      selectedFavouritePlace: selectedFavouritePlace,
-      changeFavouritePlace: changeFavouritePlace,
+    return Scaffold(
+      appBar: AppBar(
+        title: InkWell(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onTap: () {
+            showFavouritePlaceModal(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: SelectionContainer.disabled(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    favouritePlaces.isNotEmpty
+                        ? favouritePlaces[selectedFavouritePlace].getName!
+                        : defaultInstruction,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    favouritePlaces.isNotEmpty
+                        ? favouritePlaces[selectedFavouritePlace]
+                            .getFullAddress!
+                        : "",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 4,
+                    style: const TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: FilterChip(
+                      label: const Text(
+                        'Free Delivery Available',
+                        style: TextStyle(fontSize: 9),
+                      ),
+                      onSelected: (value) {},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: FilterChip(
+                      label: const Text(
+                        'Exclusive Partners',
+                        style: TextStyle(fontSize: 9),
+                      ),
+                      onSelected: (value) {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                mainAxisSpacing: 10,
+                childAspectRatio: 1,
+                children: restaurants
+                    .map((restaurant) =>
+                        RestaurantCardWidget(restaurant: restaurant))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
