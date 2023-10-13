cfb6729d406c830699629f4afd5ced0984cfac3c
K M <30794641+kam89@users.noreply.github.com>
2023-10-13 16:57:05 +0800

    Add new notification_controller

---
+++ lib/pages/notification/notification_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lightbite/models/notification_model.dart';
+import 'package:lightbite/pages/notification/notification_page.dart';
+import 'package:lightbite/providers/notification_list_provider.dart';
+import 'package:provider/provider.dart';
+
+class NotificationController extends StatefulWidget {
+  const NotificationController({super.key});
+
+  @override
+  State<NotificationController> createState() => _NotificationControllerState();
+}
+
+class _NotificationControllerState extends State<NotificationController> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<NotificationListProvider>(context, listen: false)
+        .getNotificationList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final notificationListProvider =
+        Provider.of<NotificationListProvider>(context);
+
+    return NotificationPage(
+        notificationList: notificationListProvider.notificationList);
+  }
+}
