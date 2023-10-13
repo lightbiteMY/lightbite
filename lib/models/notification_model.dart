80356bb0926640d06c903482292a5e68aec0335b
K M <30794641+kam89@users.noreply.github.com>
2023-10-13 17:53:18 +0800

    Update notification_model

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -8,14 +8,21 @@ class NotificationModel {
   final String title;
   final String subtitle;
   final String notificationId;
+  final String datetime;
+  final bool readStatus;
+  final String tag;
 
-  const NotificationModel(this.title, this.subtitle, this.notificationId);
+  const NotificationModel(this.title, this.subtitle, this.notificationId,
+      this.datetime, this.readStatus, this.tag);
 
   factory NotificationModel.fromJson(Map<String, dynamic> json) {
     return NotificationModel(
       json["title"],
       json["subtitle"],
       json["notificationId"],
+      json["datetime"],
+      json["readStatus"],
+      json["tag"],
     );
   }
 
@@ -23,5 +30,8 @@ class NotificationModel {
         "title": title,
         "subtitle": subtitle,
         "notificationId": notificationId,
+        "datetime": datetime,
+        "readStatus": readStatus,
+        "tag": tag,
       };
 }
