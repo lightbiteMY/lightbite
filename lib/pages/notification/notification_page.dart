f7823330606b476c22708d259028e54b9c3a9a0e
K M <30794641+kam89@users.noreply.github.com>
2023-10-18 18:47:47 +0800

    Update notification_page

--- lib/pages/notification/notification_page.dart
+++ lib/pages/notification/notification_page.dart
@@ -17,19 +17,36 @@ class NotificationPage extends StatelessWidget {
             (notification) => Column(
               children: [
                 ListTile(
-                  leading: notification.readStatus
-                      ? const Icon(
-                          Icons.mark_email_read,
-                          color: Colors.green,
-                        )
-                      : const Icon(
-                          Icons.mark_email_unread,
-                          color: Colors.red,
-                        ),
-                  title: Text(notification.title),
-                  subtitle: Text(notification.subtitle),
-                  trailing: Text(
-                      DateTimeModel(notification.datetime).getDisplayDateTime),
+                  title: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        notification.title,
+                        style: notification.readStatus
+                            ? const TextStyle(fontWeight: FontWeight.bold)
+                            : const TextStyle(fontWeight: FontWeight.normal),
+                      ),
+                      Text(
+                        DateTimeModel(notification.datetime).getDisplayDateTime,
+                        style: notification.readStatus
+                            ? const TextStyle(
+                                fontSize: 9, fontWeight: FontWeight.bold)
+                            : const TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text(
+                    notification.subtitle +
+                        notification.subtitle +
+                        notification.subtitle,
+                    overflow: TextOverflow.ellipsis,
+                    style: notification.readStatus
+                        ? const TextStyle(
+                            fontSize: 10,
+                          )
+                        : const TextStyle(fontSize: 10),
+                  ),
                 ),
                 // const Divider(),
               ],
